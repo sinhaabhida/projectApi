7f9c2ff4f119ff623d4779eb63fd2d0075c19f0c
sinhaabhida <abhisheksinha072@gmail.com>
2018-04-18 18:33:39 +0530

    Initial Commit

---
+++ src/main/java/com/boraji/tutorial/spring/model/Role.java
@@ -0,0 +1,34 @@
+package com.boraji.tutorial.spring.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "role")
+public class Role {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id",nullable = false)
+    private Long id;
+    @Column(name = "role",nullable = false)
+    String role;
+
+    public Role() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+   
+}
